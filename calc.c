e84c8cba413c9d98395f77b01d2bcf1b4df6aa38
workhard1111 <dldlapapdlfdlf1100@gmail.com>
2024-09-27 12:29:03 +0900

    week 4

---
+++ calc.c
@@ -0,0 +1,80 @@
+﻿#include <stdio.h>
+// #include <math.h> // M_PI는 여기에 있어야 하지만 없으면
+#include "calc.h" // 우리 만든 .h 함수
+// 큰 따옴표를 사용해야함
+
+
+
+
+//더하기
+double sum(double value1, double value2)
+{
+	return value1 + value2;
+}
+
+// 빼기(sub())
+double sub(double value1, double value2)
+{
+	return value1 - value2;
+}
+
+// 나머지
+double  mul(double value1, double value2)
+{
+	return value1 * value2;
+}
+
+// 몫
+double div(double value1, double value2)
+{
+	if (value2 != 0)
+		return value1 / value2;
+	else
+	{
+		printf("Error: Divide by 0!");
+		return 0;
+	}
+
+}
+
+// ������
+int rem(int value1, int value2)
+{
+	if (value2 != 0)
+		return value1 % value2;
+	else
+	{
+		printf("Error: Divide by 0!");
+		return 0;
+	}
+}
+
+//����
+double power(double base, double exponent)
+{
+	return pow(base, exponent);
+}
+
+//������
+double squareRoot(double num)
+{
+	if (num >= 0)
+		return sqrt(num); //math.h���� �ִ� sqrt()�Լ�
+	else
+	{
+		printf("Error: Negative number!");
+		return 1;
+	}
+}
+
+//�κ�
+double area0fCircle(double radius)
+{
+	return M_PI * radius * radius; //M_PI �����ؾ���
+}
+
+//�ѷ�
+double circumference0fCircle(double radius)
+{
+	return 2 * M_PI * radius;
+}
---
+++ clac_main.c
@@ -0,0 +1,27 @@
+﻿// calc_main.c
+#include <stdio.h>
+#include "calc.h"
+
+int main(void)
+{
+	double x = 2, y = 0;
+
+
+	printf("x: %.1f + y: %.1f = %.1f\n", x, y, sum(x, y));
+
+	printf("x: %.1f - y: %.1f = %.1f\n", x, y, sub(x, y));
+
+	printf("x: %.1f * y: %.1f = %.1f\n", x, y, mul(x, y));
+
+	printf("x: %.1f / y: %.1f = %.6f\n", x, y, div(x, y));
+
+	printf("x: %d %% y: %d = %d\n", (int)x, (int)y, rem((int)x, (int)y));
+
+	// �߰� �κ�
+	printf("x: %.1f ^ y: %.1f = %.1f", power(x, y));
+	printf("sqrt of x: %.1f = %.1f", x, squareRoot(x));
+	printf("area of x:.1f = %.1f", x, area0fCircle(x));
+	printf("circ of x: %.1f = %.1f", x, circumference0fCircle(x));
+
+	return 0;
+}
